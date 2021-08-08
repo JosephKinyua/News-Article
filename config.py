ae77c2396e3391b4e915dd763eb82b9d6ad24fd2
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-08 20:49:20 +0300

    defining the urls with epmty carlblace to be replaced

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
 import os
 class Config:
+  NEWS_DETAIL_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+
+  NEWS_BASE_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
+  
+  NEWS_API = os.environ.get('NEWS_API')
