b02ae8dfa41ed976d365a0aa591fb13ed7cf35ed
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-08 20:57:12 +0300

    helps create a dictionary config_options to help us access different configuration option classes

--- config.py
+++ config.py
@@ -1,8 +1,24 @@
 import os
-class Config:
-  NEWS_DETAIL_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
 
-  NEWS_BASE_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
+class Config:
+
+     NEWS_DETAIL_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+
+     NEWS_BASE_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
   
-  NEWS_API = os.environ.get('NEWS_API')
-  SECRET_KEY = os.environ.get('SECRET_KEY')
+     NEWS_API = os.environ.get('NEWS_API')
+     
+     SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
