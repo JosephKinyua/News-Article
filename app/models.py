890aa68cec141636d39fd63fde696f745e324417
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-09 09:48:57 +0300

    initialization of parameters

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 class Source:
     def __init__(self, id, name, description, category, url):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.category = category
+        self.url = url
     
