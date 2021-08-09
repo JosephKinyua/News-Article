30dab97d79025013af1cb105adf5b94e4c709395
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-09 10:42:16 +0300

    configuring the urls

--- app/requests.py
+++ app/requests.py
@@ -7,3 +7,9 @@ detail_url = None
 
 def configure_request(app):
     global base_url, detail_url, api_key
+    api_key = app.config['NEWS_API']
+    base_url = app.config['NEWS_BASE_URL']
+    detail_url = app.config['NEWS_DETAIL_URL']
+    
+    
+  
