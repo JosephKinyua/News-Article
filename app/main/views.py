c2e9a1438b6436b5858f4315fdc30e5c35433a15
JosephKinyua <joseph.kinyua@student.moringaschool.com>
2021-08-09 11:20:34 +0300

    define the index and the sources

--- app/main/views.py
+++ app/main/views.py
@@ -3,3 +3,11 @@ from . import main
 from ..requests import get_sources
 
 @main.route('/')
+def index():
+    general_source = get_sources('general')
+    business_source = get_sources('business')
+    ent_source = get_sources('entertainment')
+    health_source = get_sources('health')
+    science_source = get_sources('science')
+    sport_source = get_sources('sports')
+    tech_source = get_sources('technology')
